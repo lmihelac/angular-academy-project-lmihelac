d46362829144d824cc6dfa9b954943cb82b2f701
Luka Mihelac <luka.mihelac7@gmail.com>
2021-07-08 08:09:51 +0200

    Project finished

--- files/index.js
+++ files/index.js
@@ -1,26 +1,50 @@
-let reviews = ['Great show!', 'Hugh Laurie is awesome :)', 'Hope there will be 9th season!'];
+let reviews = [
+	{ comment: 'Great show!', rating: 2 },
+	{ comment: 'Hugh Laurie is awesome :)', rating: 3 },
+	{ comment: 'Hope there will be 9th season!', rating: 5 },
+];
 
 const reviewList = document.querySelector('#review-list');
 const submitBtn = document.getElementById('submitBtn');
 const inputField = document.getElementById('input-field');
 const reviewFormElement = document.querySelector('#review-form');
 
-function renderReview(input) {
+function renderReview(review) {
 	const listItem = document.createElement('li');
-	listItem.textContent = input.text;
+	const textElement = document.createElement('span');
+	const ratingElement = document.createElement('span');
+	textElement.textContent = review.comment;
+	ratingElement.textContent = review.rating;
 	reviewList.appendChild(listItem);
+	listItem.appendChild(textElement);
+	listItem.appendChild(ratingElement);
+	const createBtn = document.createElement('button');
+	createBtn.textContent = 'remove';
+	listItem.appendChild(createBtn);
+	createBtn.addEventListener('click', () => {
+		const index = reviews.indexOf(review);
+		console.log(index);
+		reviews.splice(index, 1);
+		renderAll();
+	});
 }
 
-for (let i = 0; i < reviews.length; i++) {
-	renderReview(reviews[i]);
+function renderAll() {
+	reviewList.innerHTML = '';
+	for (let i = 0; i < reviews.length; i++) {
+		renderReview(reviews[i]);
+	}
 }
+
+renderAll();
 
 reviewFormElement.addEventListener('submit', function (event) {
 	event.preventDefault();
 	const formData = new FormData(reviewFormElement);
 	const inputText = formData.get('input');
-	const todo = [inputText];
+	const inputRating = formData.get('rating');
+	const review = { comment: inputText, rating: inputRating };
 
-	reviews.push(input);
-	renderTodo(input);
+	reviews.push(review);
+	renderAll();
 });
