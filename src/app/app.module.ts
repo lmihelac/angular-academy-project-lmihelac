f095e20b6f026b54593a21cd5072296e09ce71df
Luka Mihelac <luka.mihelac7@gmail.com>
2021-07-20 23:08:19 +0200

    Move folders with moveTS extension

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainLayoutComponentComponent } from './components/main-layout-component/main-layout-component.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { AllShowsContainerComponent } from './components/all-shows-container/all-shows-container.component';
+import { AllShowsContainerComponent } from './pages/all-shows-container/all-shows-container.component';
 import { ShowListComponent } from './components/show-list/show-list.component';
 import { ShowCardComponent } from './components/show-card/show-card.component';
 import { RatingComponent } from './components/rating/rating.component';
 import { AppRoutingModule } from './app-routing.module';
-import { TopRatedShowsComponent } from './components/top-rated-shows/top-rated-shows.component';
-import { ShowDetailContainerComponent } from './components/show-detail-container/show-detail-container.component';
-import { ShowDetailComponent } from './components/show-detail/show-detail.component';
-import { MyProfileComponent } from './components/my-profile/my-profile.component';
+import { TopRatedShowsComponent } from './pages/top-rated-shows/top-rated-shows.component';
+import { ShowDetailContainerComponent } from './pages/show-detail-container/show-detail-container.component';
+import { ShowDetailComponent } from './pages/show-detail-container/components/show-detail/show-detail.component';
+import { MyProfileComponent } from './pages/my-profile/my-profile.component';
 import { ReviewListComponent } from './components/review-list/review-list.component';
 import { ReviewCardComponent } from './components/review-card/review-card.component';
+
 
 @NgModule({
 	declarations: [
@@ -34,7 +35,7 @@ import { ReviewCardComponent } from './components/review-card/review-card.compon
   MyProfileComponent,
   ReviewListComponent,
   ReviewCardComponent,
-	],
+],
 	imports: [BrowserModule, BrowserAnimationsModule, MatCardModule, MatSidenavModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
